c011b67102c3f585ccdc1bd576f88a82b0320064
Eric Whited <whited.j.eric@gmail.com>
2018-08-24 14:42:04 -0400

    init

---
+++ rubyClasses.rb
@@ -0,0 +1,67 @@
+# class Calc
+
+#     def add(val1, val2)
+#        puts val1 + val2
+#     end
+
+#     def sub(val1, val2)
+#         puts val1 - val2
+#     end
+    
+#     def mult(val1, val2)
+#        puts val1 * val2
+#     end
+
+#     def div(val1, val2)
+#         puts val1 / val2
+#     end
+
+# end
+
+# puts "hello"
+
+
+# adding = Calc.new
+# adding.add(2,2) #should give 4
+
+
+
+class Elevator 
+    attr_reader :currentFloor
+
+    def initialize
+        @currentFloor = 0
+    end
+
+    def go_up
+        @currentFloor = @currentFloor + 1
+        puts cheeryGreeting
+    end
+
+    def go_down
+        @currentFloor = @currentFloor - 1
+        puts cheeryGreeting
+    end
+
+    def cheeryGreeting
+        puts "Hello, you are currently on floor #{@currentFloor}"
+    end
+
+
+end
+
+# ride = Elevator.new 
+
+
+
+
+# class Vehicle 
+#     attr_accessor :name
+
+#     def initialize(name, numOfWheels, numOfSeats)
+#         @name = name
+#         @numOfWheels = numOfWheels
+#         @numOfSeats = numOfSeats
+#     end
+
+# end
